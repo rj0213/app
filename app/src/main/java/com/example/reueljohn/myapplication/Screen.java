dc55ec5a4fd1cac13f3f688e2bb9b2d615dc6126
Reuel John <reueljohn@MAC077.local>
2017-11-27 13:57:09 +0800

    a

---
+++ app/src/main/java/com/example/reueljohn/myapplication/Screen.java
@@ -0,0 +1,14 @@
+package com.example.reueljohn.myapplication;
+
+import android.content.Context;
+
+/**
+ * Created by reueljohn on 27/11/2017.
+ */
+
+public class Screen extends android.view.View {
+    
+    public Screen(Context context) {
+        super(context);
+    }
+}
