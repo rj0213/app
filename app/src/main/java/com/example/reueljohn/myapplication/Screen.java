6752f61b3b5b888851cd42271497713c526ca6be
Reuel John <reueljohn@MAC077.local>
2017-11-27 13:58:14 +0800

    B

--- app/src/main/java/com/example/reueljohn/myapplication/Screen.java
+++ app/src/main/java/com/example/reueljohn/myapplication/Screen.java
@@ -1,14 +1,20 @@
 package com.example.reueljohn.myapplication;
 
 import android.content.Context;
+import android.graphics.Canvas;
 
 /**
  * Created by reueljohn on 27/11/2017.
  */
 
 public class Screen extends android.view.View {
-    
+
     public Screen(Context context) {
         super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
